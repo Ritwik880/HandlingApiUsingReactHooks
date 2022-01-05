8b32408680474fd8f657a6ce7c731dfb9d751e41
Ritwik880 <rittiksinha12@gmail.com>
2022-01-05 19:58:45 +0530

    useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import Loading from './loading';
+import GithubUsers from "./githubUsers";
+import './App.css'
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getUsers = async () => {
+    try {
+      const response = await fetch('https://api.github.com/users');
+      setLoading(false);
+      setUsers(await response.json());
+    } catch (error) {
+      setLoading(false);
+      console.log("my error is " + error);
+    }
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  if (loading) {
+    return <Loading />
+  }
   return (
-    <div>
-      
-    </div>
+    <>
+      <GithubUsers users={users} />
+
+
+    </>
   )
 }
 
